218a91f16fe3eca44fa78634704571d4f9197c4b
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-11-06 20:45:14 +0900

    Updade_to_add_always_remove_console

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load',function(){
   function buildHTML(message){
-    let content = message.content? `${message.content}` : "";
     let image = message.image? `<img class='messgae_image' src=${message.image}>` : "";
     var html = `<div class="message">
                   <div class="upper-message">
@@ -16,7 +15,6 @@ $(document).on('turbolinks:load',function(){
                   </div>
                   ${image}
               </div>`
-              console.log(html)
     return html;
   }
   function scroll() {
@@ -34,16 +32,18 @@ $(document).on('turbolinks:load',function(){
       processData: false,
       contentType: false
     })
+    .always(function(){
+      $('.form__submit').prop('disabled', false);
+    })
     .done(function(message){
       var html = buildHTML(message);
       $('.messages').append(html);
-      $('.form__submit').prop('disabled', false);
       scroll()
       $('.new_message')[0].reset();
     })
     .fail(function(){
       alert('error');
-      $('.form__submit').prop('disabled', false);
+
     })
   });
 });
